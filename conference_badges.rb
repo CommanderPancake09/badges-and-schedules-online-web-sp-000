1309fa5d372689f5c7a10f98dc217273b397ceb9
Tyler Young <tyoung900@gmail.com>
2020-09-16 21:42:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
+attendees = [Edsger, Ada, Chareles, Alan, Grace, Linus, Matz]
+
 def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
-
-attendees = [Edsger, Ada, Chareles, Alan, Grace, Linus, Matz]
